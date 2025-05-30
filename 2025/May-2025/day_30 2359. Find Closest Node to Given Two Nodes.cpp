e0f44f8826ba4e6b57c5d6be486a7511caddc7e0
Gourav <98866998+gouravgodla@users.noreply.github.com>
2025-05-30 18:15:51 +0530

    Update day_30 2359. Find Closest Node to Given Two Nodes.cpp

--- 2025/May-2025/day_30 2359. Find Closest Node to Given Two Nodes.cpp
+++ 2025/May-2025/day_30 2359. Find Closest Node to Given Two Nodes.cpp
@@ -1,32 +1,41 @@
 class Solution {
- public:
-  int closestMeetingNode(vector<int>& edges, int node1, int node2) {
-    constexpr int kMax = 10000;
-    const vector<int> dist1 = getDist(edges, node1);
-    const vector<int> dist2 = getDist(edges, node2);
-    int minDist = kMax;
-    int ans = -1;
-
-    for (int i = 0; i < edges.size(); ++i)
-      if (min(dist1[i], dist2[i]) >= 0) {
-        const int maxDist = max(dist1[i], dist2[i]);
-        if (maxDist < minDist) {
-          minDist = maxDist;
-          ans = i;
+public:
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        int n = edges.size();
+        vector<vector<int>> g(n);
+        for (int i = 0; i < n; ++i) {
+            if (edges[i] != -1) {
+                g[i].push_back(edges[i]);
+            }
         }
-      }
-
-    return ans;
-  }
-
- private:
-  vector<int> getDist(const vector<int>& edges, int u) {
-    vector<int> dist(edges.size(), -1);
-    int d = 0;
-    while (u != -1 && dist[u] == -1) {
-      dist[u] = d++;
-      u = edges[u];
+        const int inf = 1 << 30;
+        using pii = pair<int, int>;
+        auto f = [&](int i) {
+            vector<int> dist(n, inf);
+            dist[i] = 0;
+            queue<int> q{{i}};
+            while (!q.empty()) {
+                i = q.front();
+                q.pop();
+                for (int j : g[i]) {
+                    if (dist[j] == inf) {
+                        dist[j] = dist[i] + 1;
+                        q.push(j);
+                    }
+                }
+            }
+            return dist;
+        };
+        vector<int> d1 = f(node1);
+        vector<int> d2 = f(node2);
+        int ans = -1, d = inf;
+        for (int i = 0; i < n; ++i) {
+            int t = max(d1[i], d2[i]);
+            if (t < d) {
+                d = t;
+                ans = i;
+            }
+        }
+        return ans;
     }
-    return dist;
-  }
 };
